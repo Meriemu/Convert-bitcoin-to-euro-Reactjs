125dc810c8184111a34502fc27a83a43dd31bbbb
mery <me.achemlal@gmail.com>
2024-03-25 14:20:15 +0100

    some updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-import './App.css';
+import "./style/__index.scss";
 // import QRcodeManagement from './QRcodeManagement.jsx'
-import BitcoinToEuro from './BitcoinToEuro.jsx'
+import BitcoinToEuro from "./BitcoinToEuro.jsx";
 function App() {
   return (
     <div className="App">
-       {/* <QRcodeManagement /> */}
-       <BitcoinToEuro />
+      {/* <QRcodeManagement /> */}
+      <BitcoinToEuro />
     </div>
   );
 }
--- src/BitcoinToEuro.jsx
+++ src/BitcoinToEuro.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Crypto from "./img/btntoeuro3.png";
 
 function BitcoinToEuro() {
   const [btcAmount, setBtcAmount] = useState(0);
@@ -26,7 +27,7 @@ function BitcoinToEuro() {
   return (
     // <div>
     //   <form onSubmit={handleSubmit} className="calculator">
-    //     <div class="btc-logo"></div>
+    //     <div className="btc-logo"></div>
     //     <label>
     //       Bitcoin Amount:
     //       <input
@@ -39,32 +40,50 @@ function BitcoinToEuro() {
     //     <button type="submit">Convert</button>
     //   </form>
 
-    //   <div class="result" id="result">
+    //   <div className="result" id="result">
     //     {" "}
     //     {eurAmount ? <p>{eurAmount} EUR</p> : null}
     //   </div>
     // </div>
 
-    <div class="calculator">
-      <div class="btc-logo"></div>
-      <div  className="input-container">
-      <form onSubmit={handleSubmit}> 
-          <input
-            type="number"
-            value={btcAmount}
-            onChange={handleChange}
-            id="btc-input"
-          />
-        <div class="convert-container">
-          <button type="submit">Convert</button>
+    <div className="Crypto__Calculator">
+      <div className="Crypto__Wrapper">
+        <div className="Crypto__Input">
+          <form onSubmit={handleSubmit}>
+            <div className="form__group field">
+              <input
+                type="input"
+                className="form__field"
+                placeholder="BTC to EURO"
+                name="btc-input"
+                value={btcAmount}
+                onChange={handleChange}
+                id="btc-input"
+                required
+              />
+              <label
+                htmlFor="name"
+                className="form__label"
+                data-text="BTC to EURO"
+              >
+                BTC to EURO
+              </label>
+            </div>
+            <div className="convert-container">
+              <button type="submit">Convert</button>
+            </div>
+          </form>
         </div>
-      </form>
+        <div className="Crypto__Result">
+          <div className="result" id="result">
+            {" "}
+            {eurAmount ? <p>{eurAmount} EUR</p> : null}
+          </div>
+        </div>
       </div>
-      <div class="result-container">
-        <div class="result" id="result">
-          {" "}
-          {eurAmount ? <p>{eurAmount} EUR</p> : null}
-        </div>
+
+      <div className="Crypto__Bg">
+        <img src={Crypto} alt="" />
       </div>
     </div>
   );
